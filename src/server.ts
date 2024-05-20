1f844b9ec48df28be0bb7d06800872786a7dca65
Rabiul Islam <rabiul.robi2023@gmail.com>
2024-05-20 18:24:11 +0600

    My Complete Server

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import app from './app'
+import config from './config'
+import mongoose from 'mongoose'
+
+async function main() {
+  try {
+    await mongoose.connect(config.DB_URL as string)
+    app.listen(config.PORT, () => {
+      console.log(`Example app listening on port ${config.PORT}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+main()
