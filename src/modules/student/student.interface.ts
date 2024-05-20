240eff11d7d9a807ad3238952a9a245df9b5a60a
Rabiul Islam <rabiul.robi2023@gmail.com>
2024-05-20 20:52:56 +0600

    Stydent Type of Typescritp Declered

---
+++ src/modules/student/student.interface.ts
@@ -0,0 +1,40 @@
+import { Schema, model, connect } from 'mongoose';
+
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+}
+
+export type StudentName = {
+    firstName: string;
+    midName: string;
+    lastName: string;
+  }
+export type LocalGuardian= {
+    localGuardianName: string;
+    localGuardianAddress: string;
+    localGuardianContactNo: string;
+
+}
+
+export type Student = {
+  id: string;
+  name: StudentName;
+  gender: 'male' | 'female';
+  dateOfBirth: string;
+  email: string;
+  avator?: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardianAddress: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg?:string;
+  isActive: "active"|"inActive";
+}
